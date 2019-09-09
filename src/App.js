e2ef6b4e6e0c2fd6336616345545d902db551474
ericauv <eriauv17@gmail.com>
2019-09-08 23:23:44 -0400

    add Line Component, first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Line from './components/Line';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Line x1={2} x2={180} y1={360} y2={520}></Line>
       </header>
     </div>
   );
---
+++ src/components/Line.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const LineStyles = styled.div`
+  border: 1px solid black;
+  width: ${props => props.args.length}px;
+  height: 0px;
+  transform: rotate(${props => props.args.angle}rad);
+  -moz-transform: rotate(${props => props.args.angle}rad);
+  -webkit-transform: rotate(${props => props.args.angle}rad);
+  -o-transform: rotate(${props => props.args.angle}rad);
+  -ms-transform: rotate(${props => props.args.angle}rad);
+  position: absolute;
+  top: ${props => props.args.y}px;
+  left: ${props => props.args.x}px;
+`;
+class Line extends Component {
+  // Calculate arguments needed to style line
+  getLineStylesArgs = (x1, y1, x2, y2) => {
+    const a = x1 - x2,
+      b = y1 - y2,
+      c = Math.sqrt(a * a + b * b);
+
+    const sx = (x1 + x2) / 2,
+      sy = (y1 + y2) / 2;
+
+    const x = sx - c / 2,
+      y = sy;
+
+    const alpha = Math.PI - Math.atan2(-b, a);
+
+    return { x, y, length: c, angle: alpha };
+  };
+  render() {
+    const { x1, x2, y1, y2 } = this.props;
+    return (
+      <LineStyles args={this.getLineStylesArgs(x1, y1, x2, y2)}></LineStyles>
+    );
+  }
+}
+
+Line.propTypes = {
+  x1: PropTypes.number,
+  x2: PropTypes.number,
+  y1: PropTypes.number,
+  y2: PropTypes.number
+};
+
+export default Line;
